26eb532fcadf4bce03b0285760c5c3fa6850ad36
Kaysser Kayyali <kaykayyali@gmail.com>
2015-07-15 13:30:07 -0400

    More Javascript exercises

--- Javascript-exercises/SpotifyArtistSearch/main.js
+++ Javascript-exercises/SpotifyArtistSearch/main.js
@@ -1,67 +1,58 @@
+'use strict';
 
 
+$('#inputArtist').on('focus', function() {
+	$('#search_results').slideToggle();
+});
 
-		$('.btn[type="submit"]').on('click', function(event){
-		event.preventDefault;
-		$('#search_results').empty();
-
-		var searchTerm = $('#inputArtist').val();
-
-		var  url = "https://api.spotify.com/v1/search?type=artist&query=" + searchTerm
-
-		var request = $.get(url, handleArtists)
-		});
-
-		function handleArtists(data) {
-			console.log(data)
-			
-
-			data.artists.items.forEach(function(artist) {
-				if (artist.images[0] != undefined){
-				$('#search_results').append('<li>'+"<button type='artist' id = '"+ artist.id + "'>" + artist.name + "<img class = 'img-circle' src='" + artist.images[0].url +"' width=10%>" +"</button>"+'</li>')
-			}
-			});
-
-		$('button[type="artist"]').on('click', function(event){
-			
+$('.btn[type="submit"]').on('click', function(event){
+	$('#search_results').slideToggle();
+	event.preventDefault;
+	$('#search_results').empty();
+	var searchTerm = $('#inputArtist').val();
+	var  url = "https://api.spotify.com/v1/search?type=artist&query=" + searchTerm
+	var request = $.get(url, handleArtists)
+});
+function handleArtists(data) {
+	console.log(data)
+	
+	data.artists.items.forEach(function(artist) {
+		if (artist.images[0] != undefined){
+			$('#search_results').append('<li>'+"<button type='artist' id = '"+ artist.id + "'>" + artist.name + "<img class = 'img-circle' src='" + artist.images[0].url +"' width=10%>" +"</button>"+'</li>')
+		}
+	});
+	$('button[type="artist"]').on('click', function(event){
+	
 		console.log(event.target.id);
-
-
-
 		var searchTerm = event.target.id
-
 		var  url = "https://api.spotify.com/v1/artists/"+ searchTerm +"/albums" 
 		console.log (url)
 		var request = $.get(url, handleAlbums)
-		});
-		};
+	});
+};
 
 function handleAlbums(data) {
 	console.log(data)
-	
 	$('#album_results').empty()
-var previousName = ""
+	var previousName = ""
 	data.items.forEach(function(album,index) {
 		if (previousName != album.name){
 
-		$('#album_results').append('<li>'+"<button type='album' id = '"+ album.id + "'>" + album.name + " <img class = 'img-circle' src='" + album.images[0].url +"' width=10%>" +"</button>"+'</li>')
-			}
-			previousName = album.name
+			$('#album_results').append('<li>'+"<button type='album' id = '"+ album.id + "'>" + album.name + " <img class = 'img-circle' src='" + album.images[0].url +"' width=10%>" +"</button>"+'</li>')
+		}
+	 previousName = album.name
 	});
 
-	$('button[type="album"]').on('click', function(event){
-			
+	$('button[type="album"]').on('click', function(event){		
 		console.log(event.target.id);
-
-
-
-		searchTerm = event.target.id
-
+		var searchTerm = event.target.id
 		var  url = "https://api.spotify.com/v1/albums/"+ searchTerm +"/tracks" 
 		console.log (url)
 		var request = $.get(url, handleTracks)
-		});
+	});
+
 	$('#albumModal').modal('toggle')
+
 };
 
 function handleTracks(data) {
@@ -69,15 +60,14 @@ function handleTracks(data) {
 	$('#albumModal').modal('toggle')
 
 	$('#tracks_results').empty()
-var previousName = ""
+	var previousName = ""
+
 	data.items.forEach(function(track) {
 		console.log(track.name)
-
-		$('#tracks_results').append('<li>' + "<a href= '" + track.preview_url + "' target='_blank'>"+ track.name +"</a>" +'</li>')
-			
-			
+		$('#tracks_results').append('<li>' + "<a href= '" + track.preview_url + "' target='_blank'>"+ track.name +"</a>" +'</li>')			
 	});
-$('#tracksModal').modal('toggle')
+
+	$('#tracksModal').modal('toggle')
 };
 
 
